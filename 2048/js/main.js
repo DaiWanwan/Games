7fd0e4c3b44e5c9e17283b4e228156d665b62228
DaiWanwan <m201672381@hust.edu.cn>
2018-05-08 22:31:16 +0800

    layout

---
+++ 2048/js/main.js
@@ -0,0 +1,17 @@
+var board = new Array();
+var score = 0;
+$(document).ready(function () {
+    newGame();
+});
+function newGame() {
+    init();
+}
+function init() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var gridCell = $('#grid-cell-' + i + "-" + j);
+            gridCell.css('top', getPosTop(i, j));
+            gridCell.css('left', getPosLeft(i, j));
+        }
+    }
+}
